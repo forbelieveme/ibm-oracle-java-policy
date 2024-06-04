ec21682439c87ebded5157b3484b8eff11bce67a
Haner Avella <havella@cysce.com>
2024-06-03 21:56:51 -0500

    Commit inicial

---
+++ OracleJavaPolicyTestJava/src/Oraclejavapolicy_JavaCompute.java
@@ -0,0 +1,178 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.apache.xalan.lib.sql.SQLQueryParser;
+
+import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbElement;
+import com.ibm.broker.plugin.MbException;
+import com.ibm.broker.plugin.MbJSON;
+import com.ibm.broker.plugin.MbMessage;
+import com.ibm.broker.plugin.MbMessageAssembly;
+import com.ibm.broker.plugin.MbOutputTerminal;
+import com.ibm.broker.plugin.MbUserException;
+
+public class Oraclejavapolicy_JavaCompute extends MbJavaComputeNode {
+
+	public void evaluate(MbMessageAssembly inAssembly) throws MbException {
+		MbOutputTerminal out = getOutputTerminal("out");
+		MbOutputTerminal alt = getOutputTerminal("alternate");
+
+		MbMessage inMessage = inAssembly.getMessage();
+		MbMessageAssembly outAssembly = null;
+
+		try {
+			// optionally copy message headers
+			// create new message as a copy of the input
+			MbMessage outMessage = new MbMessage(inMessage);
+			copyMessageHeaders(inMessage, outMessage);
+			// ----------------------------------------------------------
+			// Add user code below
+
+			Connection connection = getJDBCType4Connection(
+					"{OracleJDBCPolicy}:OraclePolicy",
+					JDBC_TransactionType.MB_TRANSACTION_AUTO);
+
+			PreparedStatement selectStatement = null;
+			String statement = "SELECT EMPLOYEE_ID, FIRST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID = 1";
+
+			selectStatement = connection.prepareStatement(statement);
+
+			ResultSet resultSet = selectStatement.executeQuery();
+
+			resultSet.next();
+
+			MbElement outRoot = outMessage.getRootElement();
+			// MbElement outJsonRoot = outRoot
+			// .createElementAsLastChild(MbJSON.PARSER_NAME);
+			// MbElement outJsonData = outJsonRoot.createElementAsLastChild(
+			// MbElement.TYPE_NAME, MbJSON.DATA_ELEMENT_NAME, null);
+			MbElement outJsonRoot = outRoot.getLastChild();
+			MbElement outJsonData2 = outJsonRoot
+					.getFirstElementByPath("/JSON/Data");
+			outJsonData2.createElementAsLastChild(MbElement.TYPE_NAME,
+					"message", "victoria");
+
+			MbElement outJsonHello = outJsonRoot
+					.getFirstElementByPath("/JSON/Data/hello");
+			String hello = outJsonHello.getValue().toString();
+			outJsonHello.setValue("hello" + hello);
+			/*
+			 * MbElement test = outJsonData.getFirstChild();
+			 * outJsonData.addAsFirstChild(test);
+			 * 
+			 * outJsonData.createElementAsLastChild(MbJSON.PARSER_NAME);
+			 * outJsonData.createElementAsLastChild(MbElement.TYPE_NAME_VALUE,
+			 * "Message", "Hello World");
+			 * 
+			 * outJsonData.createElementAsLastChild(MbElement.TYPE_NAME_VALUE,
+			 * "hello2", "world2");
+			 * outJsonData.createElementAsLastChild(MbElement.TYPE_NAME_VALUE,
+			 * "employee_id", resultSet.getInt(1));
+			 * outJsonData.createElementAsLastChild(MbElement.TYPE_NAME_VALUE,
+			 * "first_name", resultSet.getString(2));
+			 */
+			outAssembly = new MbMessageAssembly(inAssembly, outMessage);
+			// End of user code
+			// ----------------------------------------------------------
+		} catch (MbException e) {
+			// Re-throw to allow Broker handling of MbException
+			throw e;
+		} catch (RuntimeException e) {
+			// Re-throw to allow Broker handling of RuntimeException
+			throw e;
+		} catch (Exception e) {
+			// Consider replacing Exception with type(s) thrown by user code
+			// Example handling ensures all exceptions are re-thrown to be
+			// handled in the flow
+			throw new MbUserException(this, "evaluate()", "", "", e.toString(),
+					null);
+		}
+		// The following should only be changed
+		// if not propagating message to the 'out' terminal
+		out.propagate(outAssembly);
+	}
+
+	public void copyMessageHeaders(MbMessage inMessage, MbMessage outMessage)
+			throws MbException {
+		MbElement outRoot = outMessage.getRootElement();
+
+		// iterate though the headers starting with the first child of the root
+		// element, stopping before the last child (body)
+		MbElement header = inMessage.getRootElement().getFirstChild();
+		while (header != null && header.getNextSibling() != null) {
+			// copy the header and add it to the out message
+			MbElement newHeader = outRoot.createElementAsLastChild(header
+					.getParserClassName());
+			newHeader.setName(header.getName());
+			newHeader.copyElementTree(header);
+			// move along to next header
+			header = header.getNextSibling();
+		}
+	}
+
+	/**
+	 * onPreSetupValidation() is called during the construction of the node to
+	 * allow the node configuration to be validated. Updating the node
+	 * configuration or connecting to external resources should be avoided.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onPreSetupValidation() throws MbException {
+	}
+
+	/**
+	 * onSetup() is called during the start of the message flow allowing
+	 * configuration to be read/cached, and endpoints to be registered.
+	 *
+	 * Calling getPolicy() within this method to retrieve a policy links this
+	 * node to the policy. If the policy is subsequently redeployed the message
+	 * flow will be torn down and reinitialized to it's state prior to the
+	 * policy redeploy.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onSetup() throws MbException {
+	}
+
+	/**
+	 * onStart() is called as the message flow is started. The thread pool for
+	 * the message flow is running when this method is invoked.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onStart() throws MbException {
+	}
+
+	/**
+	 * onStop() is called as the message flow is stopped.
+	 *
+	 * The onStop method is called twice as a message flow is stopped. Initially
+	 * with a 'wait' value of false and subsequently with a 'wait' value of
+	 * true. Blocking operations should be avoided during the initial call. All
+	 * thread pools and external connections should be stopped by the completion
+	 * of the second call.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onStop(boolean wait) throws MbException {
+	}
+
+	/**
+	 * onTearDown() is called to allow any cached data to be released and any
+	 * endpoints to be deregistered.
+	 *
+	 * @throws MbException
+	 */
+	@Override
+	public void onTearDown() throws MbException {
+	}
+
+}
